eb97301db0cde3407bf48c5d93833674b59ec991
Eric Schulte <schulte.eric@gmail.com>
2013-09-12 15:07:40 -0600

    conditionally open trace_fd whenever needed

--- trace.c
+++ trace.c
@@ -4,7 +4,7 @@ FILE* __tracer_fd;
 
 int main(int argc, char *argv[])
 {
-  __tracer_fd = fopen("trace.out","w");
+  if (__tracer_fd == NULL) {__tracer_fd = fopen("trace.out","w");}
   fputs("LFB0\n", __tracer_fd);
   int a,b;
   a = 1;
