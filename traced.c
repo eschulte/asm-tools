00aab76469700e5e2e353d58e2a58eccb431d013
Eric Schulte <schulte.eric@gmail.com>
2013-09-12 09:03:19 -0600

    traced file in C

---
+++ traced.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+FILE* __tracer_fd;
+
+int main(int argc, char *argv[])
+{
+  __tracer_fd = fopen("trace.out","w");
+  fputs("LFB0\n", __tracer_fd);
+  int a,b;
+  a = 1;
+  b = 2;
+  printf("hello %d\n", a + b);
+  return 0;
+}
