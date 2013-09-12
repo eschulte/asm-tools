1f6e425cc1016c8a306c4706d9a75717e3482bdb
Eric Schulte <schulte.eric@gmail.com>
2013-09-12 10:03:27 -0600

    including a branch in the example file

--- plain.c
+++ plain.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
   int a,b;
   a = 1;
   b = 2;
-  printf("hello %d\n", a + b);
+  if (b > a) { printf("right %d\n", a + b); }
+  else       { printf("left  %d\n", a + b); }
   return 0;
 }
