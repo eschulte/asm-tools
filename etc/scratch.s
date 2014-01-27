c88f95f1e55e045fb7c628575c6bb1e11a5ff302
Eric Schulte <schulte.eric@gmail.com>
2014-01-27 15:54:48 -0700

    able to run to the end

--- etc/scratch.s
+++ etc/scratch.s
@@ -3,19 +3,25 @@
 
 main:
 	mov     $1, %rbx
+## Macro ostensibly start here with the following fake variables
+## first -- $2
+## second - rbx
+## cmd ---- cmp
+## mask --- $21743
 	mov     $1479682116, %eax # random number in eax (instead of call rand)
 	shr     $8, %eax # discard half random bits, and line up unused
 	and     $21743, %ax # zero out masked bits in random bits/flags
-	push    %ax         # save the random flags to the stack
+## %ax is 0x581028 5771304
+	push    %rax              # save the random flags to the stack
 	cmp     $2, %rbx
 	pushf
 	mov     $21743, %ax   # put the masked bits into eax
 	not     %ax           # negate the mask bits
-	and     %ax, 4(%rsp)  # pull masked flags into eax
+	and     %ax, 8(%rsp)  # pull masked flags into eax
 	sub     $4, %rsp      # pop flags, expose random bits on stack
-	and     %ax, 4(%rsp)  # combine saved flags and random bits
+	and     %ax, 8(%rsp)  # combine saved flags and random bits
 	sub     $4, %rsp      # pop random, expose original eax
-	xchg    %ax, 4(%rsp)  # swap eax and flags
+	xchg    %ax, 8(%rsp)  # swap eax and flags
 	popf
         ja      big
         mov     $0, %eax
