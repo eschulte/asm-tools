e47b39798a75c70317e2eb5b1886eb61908bace0
Eric Schulte <schulte.eric@gmail.com>
2014-01-27 18:01:46 -0700

    cleaned up scratch, fixed stack maint issue

--- etc/scratch.s
+++ etc/scratch.s
@@ -3,25 +3,25 @@
 
 main:
 	mov     $1, %rbx
-## Macro ostensibly start here with the following fake variables
+## Macro ostensibly start here with the following variables
 ## first -- $2
 ## second - rbx
 ## cmd ---- cmp
 ## mask --- $21743
-	mov     $1479682116, %eax # random number in eax (instead of call rand)
-	shr     $8, %eax # discard half random bits, and line up unused
-	and     $21743, %ax # zero out masked bits in random bits/flags
-## %ax is 0x581028 5771304
-	push    %rax              # save the random flags to the stack
-	cmp     $2, %rbx
-	pushf
-	mov     $21743, %ax   # put the masked bits into eax
-	not     %ax           # negate the mask bits
-	and     %ax, 8(%rsp)  # pull masked flags into eax
-	sub     $4, %rsp      # pop flags, expose random bits on stack
-	and     %ax, 8(%rsp)  # combine saved flags and random bits
-	sub     $4, %rsp      # pop random, expose original eax
-	xchg    %ax, 8(%rsp)  # swap eax and flags
+	push    %rax
+	mov     $1479682116, %eax # rand number to tweak flags
+	shr     $8, %eax          # discard 1/2 rand, and line up rest
+	and     $21743, %ax       # zero out un-masked bits in rand
+	push    %rax              # save masked rand to the stack
+	cmp     $2, %rbx          # perform the comparison
+	pushf                     # save the flags
+	mov     $21743, %ax       # put the masked bits into rax
+	not     %ax               # negate the mask bits
+	and     %ax, 8(%rsp)      # pull masked flags into rax
+	add     $8, %rsp          # pop flags, expose rand flags
+	and     %ax, (%rsp)       # combine saved flags and rand
+	add     $8, %rsp          # pop rand, expose saved rax
+	xchg    %rax, (%rsp)      # swap rax and flags, orig rax, flags on stack
 	popf
         ja      big
         mov     $0, %eax
@@ -29,3 +29,13 @@ main:
 big:
         mov     $1, %eax
         ret
+
+	## Stack preservation
+	## 0x7fffffffdef8 <- before first push
+	## 0x7fffffffdef0 <- after push %rax (saving original value)
+	## 0x7fffffffdee8 <- after push %rax
+	## 0x7fffffffdee0 <- after pushf
+	## 0x7fffffffdee8 <- after add # pop flags
+	## 0x7fffffffdef0 <- after add # pop random
+	## 0x7fffffffdef8 <- after popf
+
