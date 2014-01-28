f085d17e81ea9b9b14e5ac54b1e21877e5078a45
Eric Schulte <schulte.eric@gmail.com>
2014-01-27 23:15:17 -0700

    jumping to reliable and unreliable alternate paths

--- etc/scratch.s
+++ etc/scratch.s
@@ -1,6 +1,12 @@
 	.macro ___mk_unreliable cmd, mask, first, second
 	push    %rax              # save original value of rax
-        call    rand              # place a random number in eax
+	call    rand              # place a random number in eax
+	cmp     $127, %rax        # first 1/2 rand determines if unreliable
+	ja      .+10              # jump to reliable or unreliable track
+	pop     %rax              # /-reliable track
+	\cmd    \first, \second   # | perform the original comparison
+	pushf                     # | save original flags
+	jmp     .+45              # \-jump past unreliable track to popf
 	shr     $8, %eax          # discard 1/2 rand, and line up rest
 	and     \mask, %rax       # zero out un-masked bits in rand
 	push    %rax              # save masked rand to the stack
