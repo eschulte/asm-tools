ed1799c2ec0901a6cde3a9044dbb8e405c5c0a39
Eric Schulte <schulte.eric@gmail.com>
2014-01-27 22:43:52 -0700

    fixed stupid problem, not using original flags

--- etc/scratch.s
+++ etc/scratch.s
@@ -2,17 +2,17 @@
 	push    %rax              # save original value of rax
 	mov     $1479682116, %eax # rand number to tweak flags
 	shr     $8, %eax          # discard 1/2 rand, and line up rest
-	and     \mask, %ax        # zero out un-masked bits in rand
+	and     \mask, %rax       # zero out un-masked bits in rand
 	push    %rax              # save masked rand to the stack
 	\cmd    \first, \second   # perform the comparison
 	pushf                     # save the flags
-	mov     \mask, %ax        # put the masked bits into rax
-	not     %ax               # negate the mask bits
-	and     %ax, 8(%rsp)      # pull masked flags into rax
+	mov     \mask, %rax       # put the masked bits into rax
+	not     %rax              # negate the mask bits
+	and     (%rsp), %rax      # un-masked flags in rax
 	add     $8, %rsp          # pop flags, expose rand flags
-	and     %ax, (%rsp)       # combine saved flags and rand
+	or      (%rsp), %rax      # combine rand and saved flags
 	add     $8, %rsp          # pop rand, expose saved rax
-	xchg    %rax, (%rsp)      # swap rax and flags, orig rax, flags on stack
+	xchg    (%rsp), %rax      # swap rax and flags, orig rax, flags on stack
 	popf                      # apply flags and restore stack
 	.endm
 .text
@@ -21,7 +21,7 @@
 main:
 	mov     $35, %rax
 	mov     $1, %rbx
-        ___mk_unreliable cmp, $21743, $2, %rbx
+        ___mk_unreliable cmp, $1, $2, %rbx
         ja      big
         mov     $0, %eax
         ret
