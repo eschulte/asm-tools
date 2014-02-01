f229287d1d3b54145057860712b173ca40c96d61
Eric Schulte <schulte.eric@gmail.com>
2014-02-01 14:44:01 -0700

    scratch using /dev/random instead of random

--- etc/scratch.s
+++ etc/scratch.s
@@ -1,36 +1,33 @@
 	.macro ___mk_unreliable cmd, mask, first, second
 ___mk_ur_left_\@:
-	push    %rax              # /-88 save scratch registers
-	push    %rbx              # | 80
-	push    %rcx              # | 72
-	push    %rdx              # | 64
-	push    %rsi              # | 56
-	push    %rdi              # | 48
-	push    %r8               # | 40
-	push    %r9               # | 32
-	push    %r10              # | 24
-	push    %r11              # | 16
-	push    %r12              # | 8
-	call    random            # place a random number in eax
-	cmp     $65535, %ax       # first 1/2 rand determines if unreliable
-	pushf                     # push flags to stack
-	mov     8(%rsp), %r12     # | restore, offset by 8 from preceeding pushf
-	mov     16(%rsp), %r11    # |
-	mov     24(%rsp), %r10    # |
-	mov     32(%rsp), %r9     # |
-	mov     40(%rsp), %r8     # |
-	mov     48(%rsp), %rdi    # |
-	mov     56(%rsp), %rsi    # |
-	mov     64(%rsp), %rdx    # |
-	mov     72(%rsp), %rcx    # |
-	mov     80(%rsp), %rbx    # \- restore scratch registers
-	popf                      # restore comparison flags
-	jae     ___mk_ur_beg_\@   # jump to reliable or unreliable track
-	add     $80, %rsp         # /- reliable track: move stack pointer to rax
-	pop     %rax              # | restore rax
-	\cmd    \first, \second   # | perform the original comparison
-	pushf                     # | save original flags
-	jmp     ___mk_ur_end_\@   # \- jump past unreliable track to popf
+	push    %rax
+        push    %rsi
+        push    %rdi
+	mov     ___my_fd, %rax
+        cmp     $0, %rax
+        jne     ___mk_fd_\@
+        ## open /dev/random for reading
+	mov     $2, %rax        # sys_open
+ 	mov	$0, %rsi        # O_RDONLY
+ 	mov	$___my_fp, %rdi # file name
+        syscall
+        mov     %rax, ___my_fd
+        pop     %rdi
+        pop     %rsi
+___mk_fd_\@:
+        ## read 32 bits into %eax
+        mov     $0, %rax        # sys_read
+        mov     ___my_fd, %rdi  # file handle to read from
+        mov     $___my_rd, %rsi # read bytes into my_rd
+        mov     $4, %rdx        # length
+        syscall
+        mov     ___my_rd, %eax  # move random bytes into eax
+	cmp     $65535, %ax     # first 1/2 rand determines if unreliable
+	jae     ___mk_ur_beg_\@ # jump to reliable or unreliable track
+	pop     %rax            # /- reliable path, restore rax
+	\cmd    \first, \second # | perform the original comparison
+	pushf                   # | save original flags
+	jmp     ___mk_ur_end_\@ # \- jump past unreliable track to popf
 ___mk_ur_beg_\@:
 	add     $80, %rsp         # move stack pointer to rax
 	shr     $16, %eax         # discard 1/2 rand, and line up rest
@@ -50,22 +47,15 @@ ___mk_ur_end_\@:
 	popf                      # apply flags and restore stack
 ___mk_ur_right_\@:
 	.endm
+	.section	.data
+___my_fp:       .string "/dev/random"
+___my_fd:       .int 0
+___my_rd:       .quad 0
 .text
 .global main
 
 main:
 	mov     $2, %rbx
-	push    %rdi
-	push    %rax
-	mov     $0, %rdi
-	call    time
-	mov     %rax, %rdi
-	mov     $39, %eax
-	syscall
-	xor     %eax, %edi
-	call    srandom
-	pop     %rax
-	pop     %rdi
 	___mk_unreliable     cmp, $2261, $1, %rbx
         ja      big
         mov     $60, %rax
