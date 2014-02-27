c94558165bac41c3f8e7602cb239aa194290ce3f
Eric Schulte <schulte.eric@gmail.com>
2014-02-26 17:32:12 -0700

    macrology required to print information for dig

--- etc/scratch.s
+++ etc/scratch.s
@@ -1,67 +1,82 @@
-	.macro ___mk_unreliable cmd, mask, first, second
-___mk_ur_left_\@:
-	push    %rax
-        push    %rsi
-        push    %rdi
-	mov     ___my_fd, %rax
-        cmp     $0, %rax
-        jne     ___mk_fd_\@
-        ## open /dev/random for reading
-	mov     $2, %rax        # sys_open
- 	mov	$0, %rsi        # O_RDONLY
- 	mov	$___my_fp, %rdi # file name
+	.section	.rodata
+___dig_main:    .string "main:"
+___dig_big:     .string "big:"
+___dig_n:	.ascii "\n"
+        .section        .data
+___dig_rax:     .quad 0
+___dig_rbx:     .quad 0
+___dig_rcx:     .quad 0
+___dig_rdx:     .quad 0
+___dig_rsi:     .quad 0
+___dig_rdi:     .quad 0
+___dig_r8:      .quad 0
+___dig_r9:      .quad 0
+___dig_r10:     .quad 0
+___dig_r11:     .quad 0
+___dig_r12:     .quad 0
+        .macro ___save
+	mov     %rax, ___dig_rax
+	mov     %rbx, ___dig_rbx
+	mov     %rcx, ___dig_rcx
+	mov     %rdx, ___dig_rdx
+	mov     %rsi, ___dig_rsi
+	mov     %rdi, ___dig_rdi
+	mov     %r8,  ___dig_r8
+	mov     %r9,  ___dig_r9
+	mov     %r10, ___dig_r10
+	mov     %r11, ___dig_r11
+	mov     %r12, ___dig_r12
+        .endm
+        .macro ___restore
+	mov     ___dig_r12, %r12
+	mov     ___dig_r11, %r11
+	mov     ___dig_r10, %r10
+	mov     ___dig_r9,  %r9
+	mov     ___dig_r8,  %r8
+	mov     ___dig_rdi, %rdi
+	mov     ___dig_rsi, %rsi
+	mov     ___dig_rdx, %rdx
+	mov     ___dig_rcx, %rcx
+	mov     ___dig_rbx, %rbx
+	mov     ___dig_rax, %rax
+        .endm
+        .macro ___print value, length
+        mov     \value, %rsi
+        mov     $1, %rax         # write system call
+	mov     $2, %rdi         # STDERR file descriptor
+	mov     $\length, %rdx   # length
         syscall
-        mov     %rax, ___my_fd
-        pop     %rdi
-        pop     %rsi
-___mk_fd_\@:
-        ## read 32 bits into %eax
-        mov     $0, %rax        # sys_read
-        mov     ___my_fd, %rdi  # file handle to read from
-        mov     $___my_rd, %rsi # read bytes into my_rd
-        mov     $4, %rdx        # length
-        syscall
-        mov     ___my_rd, %eax  # move random bytes into eax
-	cmp     $65535, %ax     # first 1/2 rand determines if unreliable
-	jae     ___mk_ur_beg_\@ # jump to reliable or unreliable track
-	pop     %rax            # /- reliable path, restore rax
-	\cmd    \first, \second # | perform the original comparison
-	pushf                   # | save original flags
-	jmp     ___mk_ur_end_\@ # \- jump past unreliable track to popf
-___mk_ur_beg_\@:
-	add     $80, %rsp         # move stack pointer to rax
-	shr     $16, %eax         # discard 1/2 rand, and line up rest
-	and     \mask, %rax       # zero out un-masked bits in rand
-	push    %rax              # save masked rand to the stack
-	mov     24(%rsp), %rax    # bring original rax back for comparison
-	\cmd    \first, \second   # perform the comparison
-	pushf                     # save the flags
-	mov     \mask, %rax       # put the masked bits into rax
-	not     %rax              # negate the mask bits
-	and     (%rsp), %rax      # un-masked flags in rax
-	add     $8, %rsp          # pop flags, expose rand flags
-	or      (%rsp), %rax      # combine rand and saved flags
-	add     $8, %rsp          # pop rand, expose saved rax
-	xchg    (%rsp), %rax      # swap rax and flags, orig rax, flags on stack
-___mk_ur_end_\@:
-	popf                      # apply flags and restore stack
-___mk_ur_right_\@:
-	.endm
-	.section	.data
-___my_fp:       .string "/dev/random"
-___my_fd:       .int 0
-___my_rd:       .quad 0
+        .endm
+	.macro ___dig name, length
+        ___save
+	___print        $___dig_\name, \length
+	___print        %rax, 4
+	___print        %rbx, 4
+	___print        %rcx, 4
+	___print        %rdx, 4
+	___print        %rsi, 4
+	___print        %rdi, 4
+	___print        %r8,  4
+	___print        %r9,  4
+	___print        %r10, 4
+	___print        %r11, 4
+	___print        %r12, 4
+        ___print        $___dig_n, 1
+        ___restore
+        .endm
 .text
 .global main
 
 main:
+        ___dig  main, 5
 	mov     $2, %rbx
-	___mk_unreliable     cmp, $2261, $1, %rbx
+	cmp     $1, %rbx
         ja      big
         mov     $60, %rax
         mov     $0, %rdi
         syscall
 big:
+        ___dig  big, 4
         mov     $60, %rax
         mov     $1, %rdi
         syscall
