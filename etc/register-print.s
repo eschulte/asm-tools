83f18657fa49b1c0b5444f5acc7fac9a3a813dbb
Eric Schulte <schulte.eric@gmail.com>
2014-02-27 21:04:42 -0700

    extracted printing into a macro

--- etc/register-print.s
+++ etc/register-print.s
@@ -1,9 +1,4 @@
-        .section        .data
-buffer:         .skip 64
-
-.text
-.global main
-main:
+        .macro ___dig_printer name
         ## Register printing routine
         ## -------------------------
         ## adapted from Frank Kotler's post
@@ -16,14 +11,14 @@ main:
         ## rcx --- temporary storage
         ## rdx --- division remainder
         ## TODO: rdi --- location of destination buffer
-	mov     $2345, %rax      # put the value to write in rax
+	mov     \name, %rax    # put the value to write in rax
         mov     $10, %rbx      # divisor
         ## byte storage setup
         std                   # set direction flag to count backwards
         mov     $buffer, %rdi # buffer start
         add     $64, %rdi     #        \->end in rdx
 ### divide by 10, push remainder into buffer
-push_digit:
+push_digit_\@:
         ## divide off the largest power of ten
         xor     %rdx, %rdx      # clear rdx
         idiv    %rbx            # rdx:rax/rbx -> rax quotient, rdx remainder
@@ -36,17 +31,25 @@ push_digit:
         ## conditional loop
         mov     %rcx, %rax      # restore the quotient
         or      %rax, %rax      # is quotient zero?
-        jnz     push_digit      # if not then repeat
+        jnz     push_digit_\@   # if not then repeat
         ## print the accumulated buffer
         mov     %rdi, %rsi      # pointer to 1-before filled front of buffer
         add     $1, %rsi        # pointer to beginning of decimal
         mov     $1, %rax        # write system call
 	mov     $buffer, %rdx   # buffer start
         add     $64, %rdx       #        \->end in rdx
-        sub     %rdi, %rdx # subtract buffer start -> buffer length in rdx
-        mov     $1, %rdi   # STDOUT file descriptor
-        syscall            # print buffer contents
+        sub     %rdi, %rdx      # subtract buffer start -> buffer length in rdx
+        mov     $1, %rdi        # STDOUT file descriptor
+        syscall                 # print buffer contents
         ## return
+        .endm
+        .section        .data
+buffer:         .skip 64
+.text
+.global main
+main:
+        mov     $4321, %rax
+        ___dig_printer %rax
         mov     $60, %rax
         mov     $0, %rdi
         syscall
